63c97353759a0ff5bd67497b505f4493871e5bbb
Maestro <126394079+Maestro337@users.noreply.github.com>
2023-05-12 08:22:39 +0500

    Add files via upload

---
+++ Labs 5/Object.cpp
@@ -0,0 +1,12 @@
+﻿#include "Object.h"
+#include <iostream>
+
+Object::~Object()
+{
+	std::cout << "Âûçâàí äåñòðóêòîð îáúåêòà áàçîâîãî êëàññà Object" << std::endl;
+}
+
+Object::Object()
+{
+	std::cout << "Âûçâàí êîíñòðóêòîð îáúåêòà áàçîâîãî êëàññà Object" << std::endl;
+}
---
+++ Labs 5/Student.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "Person.h"
+class Student :
+	public Person
+{
+	string subject;
+	int mark;
+	friend istream& operator>>(istream& in, Student& s);			// ââîä
+	friend ostream& operator<<(ostream& out, const Student& s);		// âûâîä
+public:
+	Student();
+	Student(string, int, string, int);
+	~Student();
+	Student(const Student& s);
+	void Show();
+	bool is_norm();
+}; 
